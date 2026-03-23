afcb6e740b2eefd64595d5ea69f028e8f9436a69
gooseEmb <goose.embedded@gmail.com>
2026-03-23 08:26:19 -0400

    mondayex3

---
+++ 231_Team.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, count=0;
+    int a, b, c;
+    cin >> n;
+    for(int i = 0; i<n;  i++){
+        if(n>=1 && n<=1000){
+            cin >> a >> b >> c;
+            if((b==1 &  c==1) | (a==1 & b==1) | (a==1 & c==1 )| (a==1 & b==1 & c==1)){
+                count++;
+            }
+        }
+    }
+    cout << count;
+
+    return 0;
+}
