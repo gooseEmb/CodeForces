19a62ba6dbfd8b77411520837cd3fd20b704f10a
gooseEmb <goose.embedded@gmail.com>
2026-03-29 19:52:58 -0400

    saturday_29_26_is necesary use <= in while condition

---
+++ 791A_BearAndBigBrother.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+//limark<bob weight
+//limarks triple after every year
+//bob double after every year
+//limark most heavier than bob
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a, b;
+    cin >> a >> b;
+    int year = 0;
+    if(a<=b){
+        while(a <= b){
+            ++year;
+            a = a*3;
+            b = b*2;
+            
+        }
+    }
+    cout << year;
+
+    return 0;
+}
