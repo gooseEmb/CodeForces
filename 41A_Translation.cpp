a411b482e6a1c6abbfb63f5be73be3b142d1e471
gooseEmb <goose.embedded@gmail.com>
2026-04-03 05:19:26 -0400

    friday_3-4-26 to solve the problem is necesary insert an if to validate if the strings are the same size

--- 41A_Translation.cpp
+++ 41A_Translation.cpp
@@ -4,15 +4,20 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    string s, t, aux;
+    string s, t;
     int k = 0;
     bool flag = false;
     cin >> s;
     cin >> t;
     k = t.size()-1;
-    for(int j = 0; j < int(s.size()); j++){
-        if(t[k-j] != s[j])
-            flag = true;
+    if(s.size() == t.size()){
+        for(int j = 0; j < int(s.size()); j++){
+            if(t[k-j] != s[j])
+                flag = true;
+        }
+    }
+    else{
+        flag = true;
     }
     if(flag)
         cout << "NO";
