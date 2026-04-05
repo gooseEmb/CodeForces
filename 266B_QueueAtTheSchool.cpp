51efe02aa4a5e6602caee55a71515b928c7533f8
gooseEmb <goose.embedded@gmail.com>
2026-04-04 22:16:07 -0400

    saturday_4-4-26 my first solution no use library function of strings using the SWAP function it's not necesary use another string to swap, and to display a string itsn't necesary use a FOR only need write the variable using cout

--- 266B_QueueAtTheSchool.cpp
+++ 266B_QueueAtTheSchool.cpp
@@ -5,25 +5,17 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int n, t;
-    string queueOrder, newQueue;
+    string queueOrder;
     cin >> n >>t;
     cin >> queueOrder;
     for(int i = 0; i < t; i++){
         for (int j = 0; j < n; j++){
             if(queueOrder[j] == 'B' && (queueOrder[j+1]) == 'G'){
-                newQueue[j+1] = queueOrder[j];
-                newQueue[j] = queueOrder[j+1];
+                swap(queueOrder[j], queueOrder[j+1]);
                 j++;
-            }else{
-                newQueue[j] = queueOrder[j];
             }
         }
-        if(t!=1){
-            for(int l = 0; l < n; l++)
-                queueOrder[l] = newQueue[l];
-        }
     }
-    for(int i = 0; i < n; i++)
-        cout << newQueue[i];
+    cout << queueOrder;
     return 0;
 }
