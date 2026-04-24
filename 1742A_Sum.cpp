3ad94e3ab3dd41755c94997fcc427ceb0bf86ed7
gooseEmb <goose.embedded@gmail.com>
2026-04-24 08:07:36 -0400

    deleting unused variables.

--- 1742A_Sum.cpp
+++ 1742A_Sum.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n, a, b, c, min, max;
+    int n, a, b, c;
     cin >> n;
     for(int i = 0;  i < n; i++){
         cin >> a >> b >> c;
