5ff6f5f6f50b3441ae06a8a293b6bc88c60ded04
gooseEmb <goose.embedded@gmail.com>
2026-04-26 21:38:46 -0400

    saturday-26-4-26 my code has had a problem i need to learn difference between if and else if and my code has had problem using crimes variable

---
+++ 427A_PoliceRecruits.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long int n;
+    int cases = 0, recruits=0,crimes=0;
+    cin >> n;
+    int array[n] = {0};
+    for(int i = 0; i < n; i++)
+        cin >> array[i];
+    for (int j = 0; j < n; j++){
+        if((array[j] >= 1) & (array[j] <= 10) & (crimes = 0))
+        recruits = recruits + array[j];
+        else if((array[j] == -1) & (recruits >=1)){
+            recruits--;
+        }
+        else if(array[j] == -1 & recruits == 0)
+            cases++;
+        }
+    cout << cases;
+    return 0;
+}
