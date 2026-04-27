74742d626cc42125052ab67ed6adc919c82d89d5
gooseEmb <goose.embedded@gmail.com>
2026-04-26 21:42:48 -0400

    sunday-26-4-26 in my code the problem is crimes variable but if this variable is not equal to 0 never take the recruitsin the first if only need to check if the array position is between 1 and 10

--- 427A_PoliceRecruits.cpp
+++ 427A_PoliceRecruits.cpp
@@ -5,18 +5,18 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     long int n;
-    int cases = 0, recruits=0,crimes=0;
+    int cases = 0, recruits=0;
     cin >> n;
     int array[n] = {0};
     for(int i = 0; i < n; i++)
         cin >> array[i];
     for (int j = 0; j < n; j++){
-        if((array[j] >= 1) & (array[j] <= 10) & (crimes = 0))
+        if((array[j] >= 1) & (array[j] <= 10))
         recruits = recruits + array[j];
         else if((array[j] == -1) & (recruits >=1)){
             recruits--;
         }
-        else if(array[j] == -1 & recruits == 0)
+        else if((array[j] == -1) & (recruits == 0))
             cases++;
         }
     cout << cases;
