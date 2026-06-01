9c76a21626e88b81eeab81f63fc9b95c21612609
gooseEmb <goose.embedded@gmail.com>
2026-06-01 07:33:35 -0400

    monday-1-6-26 detele garbage variables

--- 2009A_Minimize.cpp
+++ 2009A_Minimize.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t, a, b, val=0,r;
+    int t, a, b;
     cin >> t;
     for (int i = 0; i < t; i++){
         cin >> a >> b;
