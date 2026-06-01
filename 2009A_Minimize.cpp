a01efc1d1bb99c85acc9276998e299749e48c9e7
gooseEmb <goose.embedded@gmail.com>
2026-06-01 07:31:37 -0400

    monday-1-6-26 no problems to implement

---
+++ 2009A_Minimize.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t, a, b, val=0,r;
+    cin >> t;
+    for (int i = 0; i < t; i++){
+        cin >> a >> b;
+        cout << b-a << endl;
+    }
+    return 0;
+}
